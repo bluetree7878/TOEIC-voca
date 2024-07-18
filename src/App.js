6cac4f3a6dc6c85726297702d2b8c9dbba13d5a2
bluetree7878 <bluetree7878@naver.com>
2024-07-18 13:25:34 +0900

    토익 영단어 CRUD 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import DayList from './component/DayList';
+import Header from './component/Header';
+import Day from './component/Day';
+import EmptyPage from './component/EmptyPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CreateWord from './component/CreateWord';
+import CreateDay from './component/CreateDay';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route path="*" element={<EmptyPage />} />
+          <Route path="/create_word" element={<CreateWord />} />
+          <Route path="/create_day" element={<CreateDay />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
